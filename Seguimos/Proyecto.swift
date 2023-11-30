b8f8516c1e21a4be3b756968e9ebf7247f492824
Pedro Omar <126366180+pedroomar23@users.noreply.github.com>
2023-11-29 23:23:42 -0500

    Add files via upload

--- Seguimos/Proyecto.swift
+++ Seguimos/Proyecto.swift
@@ -15,9 +15,9 @@ class Bank {
 }
 
 class Player {
-    var coinsInPurse: Int
+    var coinsInPurse: Int 
     init(coins: Int) {
-        coinsInPurse = Bank.distribute(coins: coins)
+        coinsInPourse = Bank.distribute(coins: coins)
     }
     func win(coins: Int) {
         coinsInPurse += Bank.distribute(coins: coins)
@@ -32,11 +32,32 @@ print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
 print("There are now \(Bank.coinsBank) coins left in the bank")
 // Prints "There are now 9900 coins left in the bank"
 
+playerOne!.win(coins: 2_000)
+print("PlayerOne won 2_000 coins & now has \(playerOne!.coinsInPurse) coins")
+// Prints "PlayerOne won 2_000 coins & now has 2100 coins"
 
+playerOne = nil
+print("PlayerOne has left the game")
+// Prints "PlayerOne has left the game"
+print("The bank now has \(Bank.coinsBank) coins")
+// Prints "The bank now 10000 coins"
 
+class Persona {
+    var residense: ?Residencia?
+}
+let john = Persona() 
 
-
-
+class Residencia {
+    var numberOfRooms = 1
+}
+let roomCout = john.residense!. numberOfRoom
+// Prints "Esto va depurar un error"
+if let roomCout = john.residense?. numberOfRoom {
+    print("La residencia de John tiene \(roomCout) habitaciones")
+} else {
+    print("No se encuentra el numero de habitaciones")
+}
+// Prints "No se encuentra el numero de habitaciones"
 
 
 
